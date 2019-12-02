16618b9fb5dd768763658a75f956e665c069872e
Ziauddin <shaikh.ziauddin@dxc.com>
2019-12-02 14:13:32 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BPS_Attrition.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
